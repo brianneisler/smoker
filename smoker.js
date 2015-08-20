785ce3ce6f8c59ee45aeea52a3ee2359d95e8e14
Brian Neisler <brian@airbug.com>
2015-08-19 22:17:18 -0700

    Update pins

--- smoker.js
+++ smoker.js
@@ -1,5 +1,5 @@
 var max31855pi = require('max31855pi');
-var listener = max31855pi(max31855pi({ cs: 18, so: 12, sck: 22 }));
+var listener = max31855pi(max31855pi({ cs: 8, so: 9, sck: 11 }));
 
 listener.on('temperature', function (temp) {
     console.log('Current temp =', temp, 'C');
